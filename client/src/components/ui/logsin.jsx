0b995d7515fcbb0d7c2de65f61c52d780c9aa2c4
Co-D-E-R <zh4223828@gmail.com>
2025-05-09 19:59:09 +0530

    changes 6

--- client/src/components/ui/logsin.jsx
+++ client/src/components/ui/logsin.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const AuthButtons = () => {
   return (
     <div className="flex justify-center space-x-4">
-      <button className="px-6 py-2 bg-slate-600 text-white font-semibold rounded-lg shadow-md hover:bg-slate-500 transition">Login</button>
+     <Link to="/login"> <button className="px-6 py-2 bg-slate-600 text-white font-semibold rounded-lg shadow-md hover:bg-slate-500 transition" >Login</button></Link>
+      <Link to="/signup">
       <button className="px-6 py-2 bg-slate-600 text-white font-semibold rounded-lg shadow-md hover:bg-slate-500 transition">Sign Up</button>
+      </Link>
     </div>
   );
 };
